4dee4afcbd58500da227c92ae84a75a6429d2733
ljh12138164 <3479261099@qq.com>
2025-03-18 16:25:40 +0800

    feat(lib)基建

---
+++ packages/lib/tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'tsup';
+type Mode = 'development' | 'production';
+// 读取环境变量
+const MODE = process.env.MODE as Mode;
+export default defineConfig({
+  // 修改入口文件为根目录的 index.ts
+  entry: ['src/**/*.ts'],
+
+  // 只输出 ESM 格式
+  format: ['esm'],
+
+  // 生成 .mjs 文件
+  target: 'esnext',
+
+  // 清理输出目录
+  clean: true,
+
+  // 生成 sourcemap
+  sourcemap:MODE==='development' && true,
+
+  // 生成声明文件
+  dts: true,
+});
