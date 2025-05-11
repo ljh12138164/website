ef4cb9a57d13f5dd9a302aa1cd0c3e36eb4d9a2c
ljh12138164 <3479261099@qq.com>
2025-05-11 16:36:37 +0800

    feat(web)登录页的初始化

--- apps/web/app/[locale]/sign-in/page.tsx
+++ apps/web/app/[locale]/sign-in/page.tsx
@@ -1,9 +1,19 @@
-export default function Page() {
+import { getTranslations } from 'next-intl/server';
+import { Input } from '@workspace/ui/components/input';
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'auth' });
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        
+    <div className='flex items-center justify-center min-h-svh'>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <h1 className='text-2xl font-bold'>{t('signInTitle')}</h1>
+        <Input />
       </div>
     </div>
-  )
+  );
 }
