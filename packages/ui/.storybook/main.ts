e0a3536d5520b61d3c916cc81089855933a6f3d3
vitality <vitality@kreator-inc.com>
2025-06-18 17:37:13 +0800

    feat(ui)编写组件库文档

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,27 +1,24 @@
-import type { StorybookConfig } from '@storybook/nextjs';
-
-import { join, dirname } from "path"
+import type { StorybookConfig } from "@storybook/nextjs";
+import "../styles/globals.css";
+import { join, dirname } from "path";
 
 /**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
+  return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    "../src/stories/**/*.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
-    getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/addon-docs'),
-    getAbsolutePath('@storybook/addon-onboarding')
-  ],
-  "framework": {
-    "name": getAbsolutePath('@storybook/nextjs'),
-    "options": {}
-  }
+  addons: ["@storybook/addon-docs"],
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {},
+  },
 };
 export default config;
+
--- packages/ui/src/stories/accordion.tsx
+++ packages/ui/src/stories/accordion.tsx
@@ -1,15 +1,15 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as AccordionPrimitive from "@radix-ui/react-accordion"
-import { ChevronDownIcon } from "lucide-react"
+import * as React from "react";
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { ChevronDownIcon } from "lucide-react";
 
-import { cn } from "@workspace/ui/lib/utils"
+import { cn } from "@workspace/ui/lib/utils";
 
-function Accordion({
+function AccordionRoot({
   ...props
 }: React.ComponentProps<typeof AccordionPrimitive.Root>) {
-  return <AccordionPrimitive.Root data-slot="accordion" {...props} />
+  return <AccordionPrimitive.Root data-slot="accordion" {...props} />;
 }
 
 function AccordionItem({
@@ -22,7 +22,7 @@ function AccordionItem({
       className={cn("border-b last:border-b-0", className)}
       {...props}
     />
-  )
+  );
 }
 
 function AccordionTrigger({
@@ -45,7 +45,7 @@ function AccordionTrigger({
         <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
-  )
+  );
 }
 
 function AccordionContent({
@@ -61,7 +61,45 @@ function AccordionContent({
     >
       <div className={cn("pt-0 pb-4", className)}>{children}</div>
     </AccordionPrimitive.Content>
-  )
+  );
 }
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+interface AccordionProps {
+  /** 触发器的属性 <a target="_blank" href="https://www.radix-ui.com/primitives/docs/components/accordion#trigger">参数</a> */
+  triggerProps?: React.ComponentProps<typeof AccordionPrimitive.Trigger>;
+  /** 内容区域的属性 <a target="_blank" href="https://www.radix-ui.com/primitives/docs/components/accordion#content">参数</a> */
+  contentProps?: React.ComponentProps<typeof AccordionPrimitive.Content>;
+  /** 项的属性 <a target="_blank" href="https://www.radix-ui.com/primitives/docs/components/accordion#item">参数</a> */
+  itemProps?: React.ComponentProps<typeof AccordionPrimitive.Item>;
+  /** 根元素的属性 <a target="_blank" href="https://www.radix-ui.com/primitives/docs/components/accordion#root">参数</a> */
+  rootProps?: React.ComponentProps<typeof AccordionPrimitive.Root>;
+  /** 数据 */
+  data: {
+    content: string | React.ReactNode;
+    header?: React.ReactNode | string;
+    key: string;
+  }[];
+}
+/**
+ *  ### 折叠面板
+ */
+function Accordion({
+  triggerProps,
+  contentProps,
+  itemProps,
+  data,
+  rootProps,
+}: AccordionProps) {
+  return (
+    <AccordionRoot type="single" className="w-full" {...rootProps}>
+      {data?.map((item) => (
+        <AccordionItem key={item.key} value={item.key || ""} {...itemProps}>
+          <AccordionTrigger {...triggerProps}>{item.header}</AccordionTrigger>
+          <AccordionContent {...contentProps}>{item.content}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </AccordionRoot>
+  );
+}
+// export { AccordionRoot, AccordionItem, AccordionTrigger, AccordionContent };
+export default Accordion;
